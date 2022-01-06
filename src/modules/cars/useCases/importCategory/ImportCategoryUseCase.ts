a67b395b2e6164899d89895a0651efbd7a15ad3c
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2022-01-05 22:12:14 -0300

    Classes refaturadas usando TSyringe e funcionando no banco de dados

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import csvParse from "csv-parse";
 import fs from "fs";
+import { inject, injectable } from "tsyringe";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IImportCategory {
@@ -7,8 +8,12 @@ interface IImportCategory {
   description: string;
 }
 
+@injectable()
 class ImportCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
 
   loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
@@ -19,20 +24,21 @@ class ImportCategoryUseCase {
 
       stream.pipe(parseFile);
 
-      parseFile.on("data", async (line) => {
-        const [name, description] = line;
-        categories.push({
-          name,
-          description,
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
         });
-      })
-      .on("end", () => {
-        fs.promises.unlink(file.path)
-        resolve(categories)
-      })
-      .on("error", (err) => {
-        reject(err);
-      })
     });
   }
 
@@ -42,15 +48,15 @@ class ImportCategoryUseCase {
     categories.map(async (category) => {
       const { name, description } = category;
 
-      const existCategory = this.categoriesRepository.findByName(name);
+      const existCategory = await this.categoriesRepository.findByName(name);
 
       if (!existCategory) {
-        this.categoriesRepository.create({ 
-          name, 
-          description
-        })
+        await this.categoriesRepository.create({
+          name,
+          description,
+        });
       }
-    })
+    });
   }
 }
 

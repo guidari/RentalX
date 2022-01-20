5b3f75024283025a73236502585e35758ef932d8
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2022-01-19 21:33:08 -0300

    Tratamento dos erros na aplicação

--- src/middlewares/ensureAuthenticate.ts
+++ src/middlewares/ensureAuthenticate.ts
@@ -1,7 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
 
 export async function ensureAuthenticate(
   request: Request,
@@ -11,24 +16,28 @@ export async function ensureAuthenticate(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error("Token missing");
+    throw new AppError("Token missing", 401);
   }
 
   const [, token] = authHeader.split(" ");
 
   try {
     // Getting the sub from the token
-    const { sub: user_id } = verify(token, "f349439cf9d3495a9c95e5759afef236");
+    const { sub: user_id } = verify(
+      token,
+      "f349439cf9d3495a9c95e5759afef236"
+    ) as IPayload;
 
     const usersRepository = new UsersRepository();
+
     const user = await usersRepository.findById(user_id);
 
     if (!user) {
-      throw new Error("User does not exists!");
+      throw new AppError("User does not exists!", 401);
     }
 
     next();
   } catch (error) {
-    throw new Error("Invalid token!");
+    throw new AppError("Invalid token!", 401);
   }
 }
